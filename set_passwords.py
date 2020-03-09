05d35265fe419bd30434d00921c14ade2d2d79b6
SCary120 <55851142+SCary120@users.noreply.github.com>
2020-03-09 13:41:29 -0400

    Update set_passwords.py

--- set_passwords.py
+++ set_passwords.py
@@ -5,9 +5,9 @@ for login in logins:
     uname = login.split()
     username = uname[0]
     password = uname[1]
-    upass = open("pass.txt", "w")
+    upass = open("pass.txt", "a")
     pw = username + ":" + password
-    upass.write(pw)
+    upass.write(pw + "\n")
     os.system("cat pass.txt |chpasswd")
 
 
